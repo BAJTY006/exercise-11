be5fe3f6ff5d6fcc4c0a442446cba767d58e72ba
Vaclav Bajtek <283148@vutbr.cz>
2026-04-22 09:40:33 +0200

    Add graphic

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 
 def random_numbers(count, low=0, high=100):
@@ -22,21 +23,33 @@ def selection_sort(values_original):
 
 values = [42, 7, 100, 91, 15, 63, 8, 57, 73, 2]
 def bubble_sort(values_original):
+    plt.ion()
+    plt.show()
     values = values_original.copy()
     n = 1
     while n < len(values):
         for i in range(len(values) - n):
             if values[i] > values[i + 1]:
                 values[i], values[i+1] = values[i+1], values[i]
+                index_highlight1 = i
+                index_highlight2 = i + 1
+                colors = ["steelblue"] * len(values)
+                colors[index_highlight1] = "tomato"
+                colors[index_highlight2] = "tomato"
+                plt.clf()
+                plt.bar(range(len(values)), values, color=colors)
+                plt.title("Bubble Sort")
+                plt.pause(0.1)
         n += 1
-
+    plt.ioff()
+    plt.show()
     return values
 
 
 
 
 if __name__ == "__main__":
-    # values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+    seznam = random_numbers(100)  # 10 čísel v rozsahu 0–100
     # print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
     #
     # small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
@@ -45,6 +58,6 @@ if __name__ == "__main__":
     # seznam = [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
     # print(selection_sort(seznam))
     # print(seznam)
-    seznam = [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
+
     print(bubble_sort(seznam))
     print(seznam)
